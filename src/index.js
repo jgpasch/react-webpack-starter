865e73e2213f6aa99747225825af955d7ae6f33b
John Paschal <jgpasch@gmail.com>
2016-10-31 15:08:19 -0400

    added top level react component, and added necessary loaders for webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 /*eslint-disable no-console*/
 
-console.log('it is working');
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
